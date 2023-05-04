64f58c25866593962b6ac0a797cffa25c408fa06
yrevna <yr.evna@mail.ru>
2023-05-04 23:08:24 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <header>
+      <Menu />
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<style>
+header {
+  height: 90px;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+}
+</style>
---
+++ src/components/MenuHp.vue
@@ -0,0 +1,79 @@
+<script setup></script>
+
+<template>
+  <menu>
+    <router-link to="/">
+      <img id="logo" alt="Лого" src="/logo_hp.png" />
+    </router-link>
+
+    <ul>
+      <li>
+        <router-link to="/catalog">Catalog</router-link>
+      </li>
+
+      <li>
+        <router-link to="/discounts">Discounts</router-link>
+      </li>
+
+      <li>
+        <router-link to="/blog">Blog</router-link>
+      </li>
+
+      <li>
+        <router-link to="/faq">FAQ</router-link>
+      </li>
+
+      <li>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+    </ul>
+  </menu>
+</template>
+
+<style>
+#logo {
+  height: 50px;
+}
+
+menu {
+  color: var(--menu-color);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
+  position: fixed;
+  backdrop-filter: blur(10px);
+  transition: backdrop-filter 0.3s ease-in-out;
+  z-index: 10;
+  box-shadow: var(--main-shadow);
+}
+
+menu > ul {
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  justify-content: space-between;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-inline-start: 0;
+}
+
+menu a {
+  text-decoration: none;
+}
+
+menu::before {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  background-color: var(--background-color);
+  left: 0;
+  top: 0;
+  opacity: 40%;
+}
+</style>
