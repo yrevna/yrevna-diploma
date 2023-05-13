cb44909d8d45f9fdad57f06e2c86539e3c5c2279
yrevna <yr.evna@mail.ru>
2023-05-13 21:18:30 +0300

    add cart store

---
+++ src/stores/cart.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+  const goods = ref([
+    { id: 1, count: 1 },
+    { id: 2, count: 4 }
+  ])
+  const itemInfo = (id) => goods.value.find((v) => v.id === id)?.count ?? 0
+  const setCount = (id, count) => {
+    const itemIndex = goods.value.findIndex((v) => v.id === id)
+    if (itemIndex === -1) {
+      if (count > 0) {
+        goods.value.push({ id, count })
+      }
+      return
+    }
+    if (count > 0) {
+      goods.value[itemIndex].count = count
+      return
+    }
+    goods.value.splice(itemIndex, 1)
+  }
+  return { goods, itemInfo, setCount }
+})
